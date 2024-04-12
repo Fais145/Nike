26f51e6e94362d2cc1281c86f48c23f1c9659e98
Faisal <faisalmaya@hotmail.com>
2024-04-12 17:22:22 +0100

    Set Up Basic Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-export default function App() {
-  return (
-    <h1 className="text-xl font-bold underline">
-      Hello very world!!
-    </h1>
-  )
-}
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from './Sections';
+
+const App = () => (
+  <main className="relative ">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding">
+        <SuperQuality/>
+      </section>
+      <section className="padding-x padding-y">
+        <Services/>
+      </section>
+      <section className="padding">
+        <SpecialOffer/> 
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews/> 
+      </section>
+      <section className="padding-x py-16 sm:py-32 w-full">
+        <Subscribe/>
+      </section>
+      <section className="padding-x padding-t pb-8 bg-black">
+        <Footer/> 
+      </section>
+  </main>
+)
+
+export default  App; 
